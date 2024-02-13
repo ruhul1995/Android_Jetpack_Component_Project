f082c83c9add804e786c64e82b6413991e2c3f83
ruhul1995 <ruhulaminchoudhury11@gmail.com>
2024-02-13 21:30:09 +0530

    Complete Example of DataBinding

--- app/src/main/java/com/example/androidjetpackcomponent/MainActivity.kt
+++ app/src/main/java/com/example/androidjetpackcomponent/MainActivity.kt
@@ -5,29 +5,54 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.androidjetpackcomponent.databinding.ActivityMainBinding
 
 /*
     Steps to How to use DataBinding in Android Project.
-    1. Enable Data Binding
-    2. Wrap Xml with layout tags.
-    3. Construct a data binding object
-    4. Eliminate findViewById() codes
+    1. We have enable Data Binding in build.gradle(app) file.
+    2. We wrap XML with layout tags for UI.
+    3. We have to create a data binding object to access it.
+    4. We will eliminate findViewById() codes.
 */
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.submit_button)
+        //Before DataBinding
+        //setContentView(R.layout.activity_main)
+
+        //After DataBinding
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //Before DataBinding
+        /*val button = findViewById<Button>(R.id.submit_button)
         button.setOnClickListener {
             displayMessage()
+        }*/
+
+        //After DataBinding
+        binding.submitButton.setOnClickListener {
+            displayMessage()
         }
+
     }
 
     private fun displayMessage() {
-        val messageView = findViewById<TextView>(R.id.welcome_text_view)
+
+        //Before DataBinding
+       /* val messageView = findViewById<TextView>(R.id.welcome_text_view)
         val nameText = findViewById<EditText>(R.id.name_edit_text)
-        messageView.text = "Hello Boss! "+ nameText.text
+        messageView.text = "Hello Boss! "+ nameText.text*/
+
+        //After DataBinding
+        //binding.welcomeTextView.text = "Hello Boss!" + binding.nameEditText.text
+
+        binding.apply {
+            welcomeTextView.text = "Hello Boss!" + nameEditText.text
+        }
     }
 }
